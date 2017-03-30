ed47b11bea3b063886d32699bf0c812717ef5568
James Graham <jngraham@u.northwestern.edu>
2017-03-30 16:59:37 -0400

    new code in MATLAB to fit exponentials to my data

--- data/large_oldV/lgt_analysis.m
+++ data/large_oldV/lgt_analysis.m
@@ -7,8 +7,30 @@ Folders = {'18_18_24', '22_22_36', '28_28_40'};
 beta = {'2.0', '2.2', '2.3'};
 beta_num = [2.0, 2.2, 2.3];
 
+N_s = 25;
+
+warning('off','all')
+warning
+
+% start fitting at time = 1, 2nd entry of time vector
+tstart = 2;
+% stop fitting at T/2-tcutoff
+tcutoff = 3;
+
+% the idea for writing the summary data is to have one file for each
+% observable, in the folder 'summary'. We'll have a separate column for
+% each size of lattice and in each row we will have the sample number
+
+% generically we will fit the exponential on times from 1 to 
+
+    
 % selects which folder to take data from / write figures to
 for i = 1:3
+    
+    mplus_mass = [];
+    mminus_mass = [];
+    flux_re_energy = [];
+    flux_im_energy = [];
     
     avg_p = figure('Name', 'Average Plaquette', 'NumberTitle', 'off');
     mplus = figure('Name', 'm0++', 'NumberTitle', 'off','PaperUnits','centimeters','PaperSize',[24 8],'PaperPosition',[-2,0.5,28,7]);
@@ -22,10 +44,18 @@ for i = 1:3
     
     for j = 1:3
         
+        temp_mplus_mass = [];
+        
+        temp_mminus_mass = [];
+        
+        temp_flux_re_energy = [];
+        
+        temp_flux_im_energy = [];
+        
         f_plaquette_name = strcat(Folders{1,i},'/plaquette_beta',beta{1,j},'.csv');
         temp = (csvread(f_plaquette_name,1));
         avg_plaquette_data = temp(:,2);
-        
+%         
         figure(avg_p)
         boxplot(avg_plaquette_data,'Whisker',0.9529);
         set(findobj(gcf,'-regexp','Tag','\w*Whisker'),'LineStyle','-','Color','k')
@@ -43,22 +73,35 @@ for i = 1:3
         mplus_data = temp(:,2:(end-1));
         
 %         for all further plots
-        T = length(mplus_data(1,:)) - 1;
-        time = 0:1:T;
+        T = length(mplus_data(1,:));
         
-        N_s = length(mplus_data(:,1));
+%         if the number of times is, for example, 24, we want time to range
+%         from 0 to 23
+        time = 0:1:(T-1);
         
         figure(mplus)
         subplot(1,3,j)
+        
         for k = 1:N_s
-            plot(time,mplus_data(k,:),'bx')
-            xlim([0 T])
             hold on;
+            tsample = time((tstart-1):(T/2-tcutoff))';
+            ysample = mplus_data(k,(tstart-1):(T/2-tcutoff))';
+            mplus_fit = fit(tsample,ysample,'exp1');
+            coeffs = coeffvalues(mplus_fit);
+            cinterval = confint(mplus_fit,0.95);
+            temp_mplus_mass = [temp_mplus_mass; [-coeffs(2), -cinterval(2,2), -cinterval(1,2)]];
+            plot(mplus_fit,tsample,ysample)
+            plot(time,mplus_data(k,:),'bx')
+            legend('off')
+            xlim([0 T])
         end
+        
         set(gca,'YTickLabelRotation',0)
         title(strcat('$$\beta=\,$$',' ',beta{1,j}),'Interpreter','latex','FontSize',16)
         xlabel('$$t$$','Interpreter','latex','FontSize',16)
         ylabel('$$\langle\Phi^\dagger(t)\Phi(0)\rangle$$','Interpreter','latex','FontSize',16)
+        
+        mplus_mass = [mplus_mass temp_mplus_mass];
         
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
@@ -69,14 +112,25 @@ for i = 1:3
         figure(mminus)
         subplot(1,3,j)
         for k = 1:N_s
-            plot(time,mminus_data(k,:),'bx')
-            xlim([0 T])
             hold on;
+            tsample = time(tstart:(T/2-tcutoff))';
+            ysample = mminus_data(k,tstart:(T/2-tcutoff))';
+            mminus_fit = fit(tsample,ysample,'exp1');
+            coeffs = coeffvalues(mminus_fit);
+            cinterval = confint(mminus_fit,0.95);
+            temp_mminus_mass = [temp_mminus_mass; [-coeffs(2), -cinterval(2,2), -cinterval(1,2)]];
+            plot(mminus_fit,tsample,ysample)
+            plot(time,mminus_data(k,:),'bx')
+            legend('off')
+            xlim([0 T])
         end
+        
         set(gca,'YTickLabelRotation',0)
         title(strcat('$$\beta=\,$$',' ',beta{1,j}),'Interpreter','latex','FontSize',16)
         xlabel('$$t$$','Interpreter','latex','FontSize',16)
         ylabel('$$\langle\Phi^\dagger(t)\Phi(0)\rangle$$','Interpreter','latex','FontSize',16)
+        
+        mminus_mass = [mminus_mass temp_mminus_mass];
       
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
@@ -87,14 +141,25 @@ for i = 1:3
         figure(flux_re)
         subplot(1,3,j)
         for k = 1:N_s
-            plot(time,flux_re_data(k,:),'bx')
-            xlim([0 T])
             hold on;
+            tsample = time(tstart:(T/2-tcutoff))';
+            ysample = flux_re_data(k,tstart:(T/2-tcutoff))';
+            flux_re_fit = fit(tsample,ysample,'exp1');
+            coeffs = coeffvalues(flux_re_fit);
+            cinterval = confint(flux_re_fit,0.95);
+            temp_flux_re_energy = [temp_flux_re_energy; [-coeffs(2), -cinterval(2,2), -cinterval(1,2)]];
+            plot(flux_re_fit,tsample,ysample)
+            plot(time,flux_re_data(k,:),'bx')
+            legend('off')
+            xlim([0 T])
         end
+        
         set(gca,'YTickLabelRotation',0)
         title(strcat('$$\beta=\,$$',' ',beta{1,j}),'Interpreter','latex','FontSize',16)
         xlabel('$$t$$','Interpreter','latex','FontSize',16)
         ylabel('$$\textrm{Re}\left(\langle\Phi^\dagger(t)\Phi(0)\rangle\right)$$','Interpreter','latex','FontSize',16)
+        
+        flux_re_energy = [flux_re_energy temp_flux_re_energy];
         
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
@@ -105,20 +170,32 @@ for i = 1:3
         figure(flux_im)
         subplot(1,3,j)
         for k = 1:N_s
-            plot(time,flux_im_data(k,:),'bx')
-            xlim([0 T])
             hold on;
+            tsample = time(tstart:(T/2-tcutoff))';
+            ysample = flux_im_data(k,tstart:(T/2-tcutoff))';
+            flux_im_fit = fit(tsample,ysample,'exp1');
+            coeffs = coeffvalues(flux_im_fit);
+            cinterval = confint(flux_im_fit,0.95);
+            temp_flux_im_energy = [temp_flux_im_energy; [-coeffs(2), -cinterval(2,2), -cinterval(1,2)]];
+            plot(flux_im_fit,tsample,ysample)
+            plot(time,flux_im_data(k,:),'bx')
+            legend('off')
+            xlim([0 T])
         end
         set(gca,'YTickLabelRotation',0)
         title(strcat('$$\beta=\,$$',' ',beta{1,j}),'Interpreter','latex','FontSize',16)
         xlabel('$$t$$','Interpreter','latex','FontSize',16)
         ylabel('$$\textrm{Im}\left(\langle\Phi^\dagger(t)\Phi(0)\rangle\right)$$','Interpreter','latex','FontSize',16)
         
-%         close all;
+        flux_im_energy = [flux_im_energy temp_flux_im_energy];
         
     end
    
     
+%     save all figures to file, save all summaries to file (as .mat with no column names...)
+    summaryname = strcat('summary/', Folders{1,i});
+    save(summaryname, 'avg_plaquette_data', 'mplus_mass', 'mminus_mass', 'flux_re_energy', 'flux_im_energy');
+
     print(mplus,strcat('figures/',Folders{1,i},'/mplus.pdf'),'-dpdf','-r300')
     print(mminus,strcat('figures/',Folders{1,i},'/mminus.pdf'),'-dpdf','-r300')
     print(flux_re,strcat('figures/',Folders{1,i},'/flux_re.pdf'),'-dpdf','-r300')
